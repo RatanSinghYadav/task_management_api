0d8cad26279f6b9cf4c7ceb76e0f47fe87abdf79
Ratan Singh Yadav <108169130+RatanSinghYadav@users.noreply.github.com>
2024-08-30 15:25:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 8000
 
 const corsOption = {
-    // origin: "http://localhost:3000",
+    origin: "https://task-management-webapp-kr7n.onrender.com/",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie', 'Token']
